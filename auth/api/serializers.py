84748bb426d8193feafe7f829fcde050ad6553cc
Luks <sluckny@gmail.com>
2024-04-04 20:06:16 -0400

    fix: password hashing

--- auth/api/serializers.py
+++ auth/api/serializers.py
@@ -12,7 +12,11 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     id = serializers.UUIDField(read_only=True)
-        
+    
+    def create(self, validated_data):
+            user = User.objects.create_user(email=validated_data['email'], password=validated_data['password'])
+            return user
+    
     class Meta: 
         model = User
         fields = ['id', 'email', 'password']
