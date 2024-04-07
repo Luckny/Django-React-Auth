39f75eb164f1e5fa9b194a56d0a59cfe3f42cd93
Luks <sluckny@gmail.com>
2024-04-07 16:53:28 -0400

    feat: custom error message for duplicate user email

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -8,11 +8,17 @@ source: https://www.django-rest-framework.org/api-guide/serializers/
 
 from rest_framework import serializers
 from .models import User
+from rest_framework.validators import UniqueValidator
 
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     id = serializers.UUIDField(read_only=True)
+    email = serializers.EmailField(
+        validators=[
+            UniqueValidator(queryset=User.objects.all(), message="user already exist")
+        ]
+    )
 
     def create(self, validated_data):
         user = User.objects.create_user(
