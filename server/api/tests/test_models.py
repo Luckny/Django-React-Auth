0342e5ba7073c2e2ff7eddcb6dbdfd50deff84c6
Luckny <sluckny@gmail.com>
2024-05-04 10:18:21 -0400

    test: Models 100%

--- server/api/tests/test_models.py
+++ server/api/tests/test_models.py
@@ -1,22 +1,89 @@
-from rest_framework.test import APITestCase
-from api.models import User
-from api.models import OneTimePassword
+import datetime
+from django.utils import timezone
+from django.test.utils import tag
+from django.test import TestCase
+from api.models import User, OneTimePassword, five_minutes
 
 
-class UserModelTest(APITestCase):
-    default_email = "user@email.com"
-    default_pass = "password1!"
+class UserTestCase(TestCase):
+    valid_email = "valid@email.com"
+    valid_password = "secret123"
 
-    # add user with default email and default password to database
-    def create_default_user(self):
-        # user in database
-        user = User.objects.create_user(
-            email=self.default_email, password=self.default_pass
+    def setUp(self):
+        method = getattr(self, self._testMethodName)
+        tags = getattr(method, "tags", {})
+        if "skip_setup" in tags:
+            return  # setUp skipped
+
+    @tag("skip_setup")
+    def test_create_user_valid(self):
+        User.objects.create_user(email=self.valid_email, password=self.valid_password)
+
+        # user should exist in db
+        user = User.objects.filter(email=self.valid_email).first()
+        self.assertIsNotNone(user)
+
+    def test_create_user_missing_required_fields(self):
+        # no email should raise value error
+        with self.assertRaises(ValueError):
+            User.objects.create_user(email=None, password=self.valid_password)
+
+        # no password should raise value error
+        with self.assertRaises(ValueError):
+            User.objects.create_user(email=self.valid_email, password=None)
+
+    def test_create_super_user_working(self):
+        User.objects.create_superuser(
+            email=self.valid_email, password=self.valid_password
         )
-        return user
 
-    def test_otp_str(self):
-        user = self.create_default_user()
-        otp = OneTimePassword.objects.create(user=user)
+        # filter returns a list of users
+        user = User.objects.filter(email=self.valid_email).first()
+        # should be defined
+        self.assertIsNotNone(user)
+        # should be super user
+        self.assertTrue(user.is_superuser)
+        # should have staff privilege
+        self.assertTrue(user.is_staff)
 
+    def test_add_five_minutes(self):
+        expected_time = timezone.localtime(timezone.now()) + datetime.timedelta(
+            minutes=5
+        )
+        result = five_minutes()
+        self.assertAlmostEqual(
+            result, expected_time, delta=datetime.timedelta(seconds=1)
+        )
+
+
+# class OneTimePassword(models.Model):
+#     # random unique field of 6 characters
+#     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+#     code = models.CharField(max_length=6, default=secrets.token_hex(3).upper())
+#     # related user
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     # OTP expires after 5 minutes of creation
+#     expires_at = models.DateTimeField(default=five_minutes)
+#
+#     def __str__(self) -> str:
+#         return f"{self.code}"
+class OneTimePasswordTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email="valid@email.com", password="secret123"
+        )
+
+    def test_create_otp(self):
+        otp = OneTimePassword.objects.create(user=self.user)
+        self.assertIsNotNone(otp)
+        self.assertEqual(otp.user, self.user)
+        # should have length of 6 characters
+        self.assertEqual(len(otp.code), 6)
+
+    def test_otp_string_representation(self):
+        # Create an OTP instance
+        otp = OneTimePassword.objects.create(user=self.user)
+
+        # Check if __str__ method returns the code
         self.assertEqual(str(otp), otp.code)
